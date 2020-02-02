11e23eba887f6c368e127be8b40cfb726240d479
ncaudill27 <nelson.a.caudill@gmail.com>
2020-02-01 23:32:01 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_name
+    self.user.username
+  end
+
+  def user_attributes=(user_hash)
+    unless user_hash[:username] == ""
+      user = User.create(user_hash)
+      self.user_id = user.id
+    end
+  end
 end
